a00a93fd5f689b200987bb64c4fdbecb5dc8915c
Pedro Venâncio dos Santos <pedrosantos121229@gmail.com>
2025-05-26 22:23:41 -0300

    fix: corrigindo para se adequar a todos os casos

--- src/Longest_Substring.java
+++ src/Longest_Substring.java
@@ -7,10 +7,11 @@ public class Longest_Substring {
 
         for (int i = 0; i < s.length(); i++) {
             if(letras.contains(s.charAt(i))){
-                letras.clear();
+                //letras.clear();
+                letras = new ArrayList<>(letras.subList(letras.indexOf(s.charAt(i)) + 1, letras.size()));
             }
             letras.add(s.charAt(i));
-            int length_aual = letras.stream().toArray().length;
+            int length_aual = letras.size();
 
             if(length_aual > maior_length){
                 maior_length = length_aual;
@@ -22,8 +23,8 @@ public class Longest_Substring {
     public static  void main(String[] args){
          Longest_Substring ls = new Longest_Substring();
 
-         int resposta =  ls.LongestSubstring("bbbbb");
+         int resposta =  ls.LongestSubstring("pwwkew");
 
-         System.out.printf(Integer.toString((resposta)));
+         System.out.println(resposta);
     }
 }
